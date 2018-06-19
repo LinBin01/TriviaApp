e607cc5d549ddc883827b90e0deb98b517320e80
Bin Lin <binlin2000@gmail.com>
2018-06-19 15:00:34 -0400

    created fragments for question creator

--- app/src/main/java/com/example/binlin/triviaapp/MainActivity.java
+++ app/src/main/java/com/example/binlin/triviaapp/MainActivity.java
@@ -3,11 +3,36 @@ package com.example.binlin.triviaapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    private QuestionCreatorFragment questionCreatorFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.add_question_button)
+    protected void addQuestionClicked(){
+        questionCreatorFragment = questionCreatorFragment.newInstance();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_holder, questionCreatorFragment).commit();
+    }
+
+    @OnClick(R.id.take_quiz_button)
+    protected void takeQuiz(){
+
+    }
+
+    @OnClick(R.id.delete_quiz_button)
+    protected void deleteQuiz(){
+
+    }
+
+
 }
